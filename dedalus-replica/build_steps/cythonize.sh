0cd1d8b061c2fb638d4dcbbb176d4a2129c0f38a
Will Sheehan <wsheehan@bates.edu>
2017-03-27 20:21:23 -0400

    Source and cd to correct path in cythonize step

--- dedalus-replica/build_steps/cythonize.sh
+++ dedalus-replica/build_steps/cythonize.sh
@@ -1,4 +1,9 @@
+# get dedalus source
+source /home/dedalus/dedalus/bin/activate
+
 # compile cython with setup.py
 export MPI_PATH=/home/dedalus/dedalus
 export FFTW_PATH=/home/dedalus/dedalus
+
+cd /home/dedalus/dedalus/src/dedalus/
 python3 setup.py build_ext --inplace
